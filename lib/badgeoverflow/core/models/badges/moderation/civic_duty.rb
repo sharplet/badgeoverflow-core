4e270c25e918710dd8c68068eeb3100623300dde
Stephanie Sharp <stephrsharp@gmail.com>
2013-09-15 21:25:15 +1000

    Implement Civic Duty badge

--- lib/badgeoverflow/core/models/badges/moderation/civic_duty.rb
+++ lib/badgeoverflow/core/models/badges/moderation/civic_duty.rb
@@ -4,7 +4,44 @@ class CivicDuty < Badge
   def series
     :civic_duty
   end
+
+  def progress_title
+    "You're getting close to..."
+  end
+
+  def calculate_progress!
+    user = service.fetch('users', {
+      ids: user_id,
+      sort: 'reputation',
+      order: 'desc',
+      filter: '!--iqJa71*_pa',
+      pagesize: 1,
+      fetch_all_pages: false
+    }).first
+
+    vote_count = user['up_vote_count'] + user['down_vote_count']
+    remaining = required_votes - vote_count
+
+    remaining_str = "#{remaining} " + "vote".pluralize(remaining, "votes")
+
+    self.progress_description = "#{description}. Only #{remaining_str} to go!"
+  end
+
+  def required_votes
+    300
+  end
+
+  def service
+    @service ||= StackExchangeService.new('stackoverflow')
+  end
 end
 
 class Electorate < CivicDuty
+  def progress_title
+    "Have you considered..."
+  end
+
+  def calculate_progress!
+    self.progress_description = description
+  end
 end
